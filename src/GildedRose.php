931e0d914525912be2db38474d07395c5acb340d
ttnppedr <priest0240@gmail.com>
2018-08-01 14:57:17 +0800

    refactor Aged Brie

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -27,6 +27,10 @@ class GildedRose
             return $this->normalTick();
         }
 
+        if ($this->name == 'Aged Brie') {
+            return $this->brieTick();
+        }
+
         if ($this->name != 'Aged Brie' and $this->name != 'Backstage passes to a TAFKAL80ETC concert') {
             if ($this->quality > 0) {
                 if ($this->name != 'Sulfuras, Hand of Ragnaros') {
@@ -87,6 +91,21 @@ class GildedRose
 
         if ($this->sellIn <= 0) {
             $this->quality -= 1;
+        }
+    }
+
+    protected function brieTick()
+    {
+        $this->sellIn -= 1;
+
+        if ($this->quality >= 50) {
+            return;
+        }
+
+        $this->quality += 1;
+
+        if ($this->sellIn <= 0 && $this->quality < 50) {
+            $this->quality += 1;
         }
     }
 }
