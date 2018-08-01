577faec0fc0563278a508c87783e328788903731
ttnppedr <priest0240@gmail.com>
2018-08-01 14:40:03 +0800

    refactor normal items

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -23,6 +23,10 @@ class GildedRose
 
     public function tick()
     {
+        if ($this->name == 'normal') {
+            return $this->normalTick();
+        }
+
         if ($this->name != 'Aged Brie' and $this->name != 'Backstage passes to a TAFKAL80ETC concert') {
             if ($this->quality > 0) {
                 if ($this->name != 'Sulfuras, Hand of Ragnaros') {
@@ -68,6 +72,21 @@ class GildedRose
                     $this->quality = $this->quality + 1;
                 }
             }
+        }
+    }
+
+    protected function normalTick()
+    {
+        $this->sellIn -= 1;
+
+        if ($this->quality == 0) {
+            return;
+        }
+
+        $this->quality -= 1;
+
+        if ($this->sellIn <= 0) {
+            $this->quality -= 1;
         }
     }
 }
